fe569bb5cee814dbaae9ca3fa9eefacaa5996d96
Hallam Rear <hallamrear@gmail.com>
2026-05-31 20:58:17 +0100

    Update Quickhull.cpp

--- Artifact/Physics/Quickhull/Quickhull.cpp
+++ Artifact/Physics/Quickhull/Quickhull.cpp
@@ -937,6 +937,7 @@ void ConvexHull::AddFaceToHull(ConvexHullFace* face)
 		ConvexHullFace* back = FacesListHead->Prev;
 		back->Next = face;
 		face->Prev = face;
+		face->Prev = back;
 	}
 	else
 	{
@@ -955,6 +956,7 @@ void ConvexHull::AddEdgeToHull(ConvexHullHalfEdge* edge)
 		ConvexHullHalfEdge* back = EdgesListHead->Prev;
 		back->Next = edge;
 		edge->Prev = edge;
+		edge->Prev = back;
 	}
 	else
 	{
